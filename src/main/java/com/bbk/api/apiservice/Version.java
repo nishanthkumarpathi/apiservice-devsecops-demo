09c39b68dcd2a7ff69b92ff8535d833496581283
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2023-01-18 10:49:31 +0300

    added version in the code

---
+++ src/main/java/com/bbk/api/apiservice/Version.java
@@ -0,0 +1,13 @@
+package com.bbk.api.apiservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Version {
+
+	@GetMapping("/version")
+	public String version() {
+		return "REPLACETHISWITHVERSIONUSINGGITHURUNNER";
+	}
+}
